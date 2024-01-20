74e47e44796c7c611dc7c1a7d638064ad5067c8c
linzhongjiu <linzhongjiu@outlook.com>
2024-01-20 17:24:13 +0800

    add gin freamwork

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-// 启动类
-func main() {
-	r := gin.Default() //携带基础中间件启动
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello gin!",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+func Index(context *gin.Context) {
+	context.String(200, "Hello world")
 
 }
+
+// web框架gin启动类
+func main() {
+	r := gin.Default() //携带基础中间件启动
+	//
+	r.GET("/test", Index)
+
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+
+}
+
+// 连接mysql
